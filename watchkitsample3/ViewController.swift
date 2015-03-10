059b14cbfa3268de40e0ba6f70547d95f8e8ad39
平塚俊輔 <shun_hiratsuka2002@yahoo.co.jp>
2015-03-11 07:40:05 +0900

    Initial Commit

---
+++ watchkitsample3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  watchkitsample3
+//
+//  Created by HIRATSUKA SHUNSUKE on 2015/03/11.
+//  Copyright (c) 2015年 HIRATSUKA SHUNSUKE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
